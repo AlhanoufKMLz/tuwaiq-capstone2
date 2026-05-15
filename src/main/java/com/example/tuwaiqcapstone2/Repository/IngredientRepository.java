28dcd3bb52841249b56192e1726b7502ab3924b7
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-05-15 14:26:34 +0300

    feat: extra endpoints for ingredient

--- src/main/java/com/example/tuwaiqcapstone2/Repository/IngredientRepository.java
+++ src/main/java/com/example/tuwaiqcapstone2/Repository/IngredientRepository.java
@@ -4,8 +4,12 @@ import com.example.tuwaiqcapstone2.Model.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
 
     Ingredient findIngredientById(Integer id);
+
+    List<Ingredient> findIngredientByRecipeId(Integer recipeId);
 }
